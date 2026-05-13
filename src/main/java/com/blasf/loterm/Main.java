7d9c0ef967dcd18e8963de03cc3ad8403de77a53
Blas Fernández <elite.atencionalcliente@gmail.com>
2026-05-13 18:45:17 +0200

    Added license to source code

--- src/main/java/com/blasf/loterm/Main.java
+++ src/main/java/com/blasf/loterm/Main.java
@@ -1,3 +1,21 @@
+/*
+ * Remote terminal server made with Java.
+ * Copyright (C) 2026  Blas Fernández
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.blasf.loterm; // Define the package name
 
 import java.io.*; // Import I/O classes for reading/writing
